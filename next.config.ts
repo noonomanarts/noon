ddb847af2271ccba539df242469b312fe61732ca
Milad Raeisi <6504337+miladsoft@users.noreply.github.com>
2026-04-01 09:00:24 +0400

    feat: add outputFileTracingExcludes configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  outputFileTracingExcludes: {
+    "*": ["./.wwebjs_auth/**/*", "./.wwebjs_cache/**/*"],
+  },
 };
 
 export default nextConfig;
